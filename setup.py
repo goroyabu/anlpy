3154aa1e63a84b06123990b02ca3ec61969397c5
goroyabu <goro.yabu@ipmu.jp>
2020-03-09 00:58:17 +0900

    setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils.version import LooseVersion
 
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
+    def __init__(self, name, sourcedir='source'):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
